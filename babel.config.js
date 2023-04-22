0047f1be7effb6db24fb2a938b86b83c90ed3eb5
Alexandre Medeiros <alxmedeiros@gmail.com>
2023-04-22 19:32:57 -0300

    Preparing the base repo

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,31 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@/components': './src/components',
+            '@/assets': './src/assets',
+            '@/theme': './src/theme',
+            '@/lib': './src/lib',
+            '@/services': './src/services',
+            '@/utils': './src/utils',
+            '@/contexts': './src/contexts',
+            '@/hooks': './src/hooks',
+          },
+        },
+      ],
+    ],
   };
 };
